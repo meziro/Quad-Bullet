5b2fc2bc05acf7d9fbe1b4c6832a1c3fde10b1d8
Iwancof <rock.iwancof@gmail.com>
2019-09-24 09:28:24 +0900

    ロジック組み直し

--- ArduinoBaseSystem/ArduinoBaseSystem.ino
+++ ArduinoBaseSystem/ArduinoBaseSystem.ino
@@ -1,11 +1,8 @@
-String string_buf;
-//char *char_buf;
-
-int outputs[] = {2,3,4,5,14,15,16,17,18,19,0};
-int output_num = 11;
+int outputs[] = {2,3,4,5,14,15,16,17,18,19};
+int output_num = 10;
 int inputs[] = {6};
 int input_num = 1;
-int *prev;
+int *switch_state_prev;
 
 /*  Output pin details
  *  
@@ -23,7 +20,7 @@ int *prev;
 void setup() {
   //serial init
   Serial.begin(115200);
-  Serial.setTimeout(0.5);
+  Serial.setTimeout(1);
 
   Serial.println("[DEBUG]Serial port had beed initialized");
   
@@ -41,16 +38,17 @@ void setup() {
 
   //data init
   //char_buf = (char*)malloc(sizeof(char) * 256);
-  prev = (int*)calloc(sizeof(int),input_num);
+  switch_state_prev = (int*)calloc(sizeof(int),input_num);
   //Full with 0(by using calloc)
 
   digitalWrite(19,LOW);
 
-  delay(2000);
+  delay(1000);
   Serial.println("[DEBUG]Ready!");
 }
 
-int selector = 0;
+String Onetime_Control_Message = ""; //1つのシリアル通信で送られるメッセージの置き場
+String Complate_Control_Message = ""; //1つの命令っメッセージ
 
 void loop() {
   //Send data
@@ -58,82 +56,93 @@ void loop() {
   String message = "";
   for(int i = 0;i < input_num;i++) {
     int tmp = digitalRead(inputs[i]);
-    if(tmp != prev[i]) {
+    if(tmp != switch_state_prev[i]) {
       message += "status_updata:{" + String(inputs[i]) + "," + String(tmp) + "};";
     }
-    prev[i] = tmp;
+    switch_state_prev[i] = tmp;
   }
 
   if(message.length() != 0) {
     Serial.println(message);
   }
 
-
   //Receive data
   
   if (Serial.available() <= 0) return; //no data that we should receive
 
-  string_buf = Serial.readString(); //receive data
+  //string_buf = Serial.readString(); //receive data
+  Onetime_Control_Message = Serial.readString();
+  int s_len = Onetime_Control_Message.length();
 
-  int lastest = 0;
-  int light_status = HIGH;
-  
-  for (int read_count = 0; read_count < string_buf.length(); read_count++) {
-    switch (string_buf[read_count]) {
-      case ',': {
-        String ss = string_buf.substring(lastest,read_count + 1); 
-        lastest = read_count + 1;
-        
-        digitalWrite(19,light_status);
-        Serial.println(String(light_status));
-        control(ss.toInt());
+  int read_count = 0;
+  int prev = 0;
+  for(auto it = Onetime_Control_Message.begin();it != Onetime_Control_Message.end();it++) {
+     if(*it == '*') { //命令が終わった
+      Complate_Control_Message += Onetime_Control_Message.substring(prev,read_count + 1);
 
-        delay(15);
-        
-        /*
-        Serial.print(String(ss.toInt(),10)[0]);
-        Serial.print("を、状態");
-        Serial.print(String(light_status,10)[0]);
-        Serial.println("にします");
-
-        digitalWrite(ss.toInt(),light_status);
-        */
-        
-        break;
-      }
-      case '#': {
-        light_status = light_status ? LOW : HIGH;
-        lastest = read_count + 1;
-        break;
-      }
-      default :{
-        //Serial.write("error");
-        
-        break;
+      Tokenize_Control_Message(Complate_Control_Message);
+      
+      Complate_Control_Message = "";
+      prev = read_count + 1;
+    } else if(read_count == s_len - 1) { //終端文字なしで1つのシリアル通信が終了
+      String tmp = Onetime_Control_Message.substring(prev,read_count + 1);
+      if(tmp != "\n") {
+        Complate_Control_Message += Onetime_Control_Message.substring(prev,read_count + 1);
       }
     }
+    read_count++;
   }
+  
+}
+
+int light_state = HIGH; //ライトの状態
+
+void Tokenize_Control_Message(String mes) {
+  int len = mes.length(); //文字列の長さ(Stringはリンクリストなので、lengthがO(N)のため)
+
+  int last = 0,now = 0;//csvを読むやつ
+  //Read as csv
+
+  for(auto it = mes.begin();it != mes.end();it++) {
+    //swtich文を使いたかったけど、可能な実装方法が汚かったので残念ながらif
+    char c = *it;
+    if((c == ',' || c == '*' || c == '~') && (1 <= now - last)) { //直前までの文字が数値なので解析するやつ
+      int num = mes.substring(last,now).toInt();
+      last = now + 1;
+
+      Control(num); //ピンの状態をもとになにかいいことしてくれるはずだと思う。多分。おそらく(予定)。
+      Serial.println(String(num) + " to " + (light_state ? "HIGH" : "LOW"));
+    }
+    if(c == '~') {
+      light_state = light_state ? LOW : HIGH;
+      last = now + 1;
+    }
+    
+    now++; //index
+  }
+  light_state = HIGH; //初期化
 }
 
 
-void control(int x) {
-  if(0 <= x && x <= 23) {
-    digitalWrite(0,LOW);
-    
-    //Serial.println(String(x));
-    int *ptr = Encode(x);
-    String tmp = "";
-    for(int i = 0;i < 5;i++) {
-      digitalWrite(i + 14,ptr[i]);
-      //Serial.println(String(ptr[i]));
-      tmp += " " + String(ptr[i]);
-    }
-
-    Serial.println(tmp);
-    //Serial.println("");
-    free(ptr);
-  } else {
+void Control(int x) {
+  if(!(0 <= x && x <= 23)) //範囲外だったら
+    return;
+  
+  digitalWrite(19,light_state); //指示しているライトの状態を指定
+  digitalWrite(2,HIGH); //Enable
+  
+  int *ptr = Encode(x); //二進数列に変換
+  String tmp = ""; //デバック用メッセージリスト
+  for(int i = 0;i < 5;i++) {
+    digitalWrite(i + 14,ptr[i]); //Analog pinにbinaryを出力
+    //Serial.println(String(ptr[i]));
+    tmp += " " + String(ptr[i]);
   }
+
+  //Serial.println(tmp);
+  
+  free(ptr);
+  delay(25); //サブがメッセージを理解する猶予
 }
 
 
--- Wrapper.py
+++ Wrapper.py
@@ -8,7 +8,7 @@ class Arduino_Controler:
 
     def __init__(self,com_port) :
         #Serial port を初期化
-        self.Serial = serial.Serial(com_port,115200,timeout=0.5)
+        self.Serial = serial.Serial(com_port,115200,timeout=1)
 
         #各種変数を初期化
         self.swtich = {
@@ -30,12 +30,18 @@ class Arduino_Controler:
 
         thread.start()
 
+        self.message = "~0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24*"
+
+        time.sleep(1)
+
     def timer(self) :
         while self.exit_code :
             #Receive data
             s = self.Serial.readline().decode('utf-8')
 
-            result = re.findadd("status_update:{([0-9]+),([0-9]+)};",s)
+            if(len(s) != 0) :
+                print(s)
+            result = re.findall("status_update:{([0-9]+),([0-9]+)};",s)
 
             if result:
                 for x in result:
@@ -44,16 +50,24 @@ class Arduino_Controler:
             #Send data
             if(self.message == "") :
                 continue
+            
+            print(self.message + "*")
 
-            self.Serial.write(self.message.encode())
-            self.message = "#0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24"
+            self.Serial.write((self.message + "*").encode())
+            self.message = ""
+
+            print("sent")
 
     def wait_arduino(self) :
         for i in range(10000000000) :
-            s = self.Serial.readline().decode('utf-8')
-        print(s)
-        if("Ready" in s) :
-            return
+            s = 0
+            try:
+                s = self.Serial.readline().decode('utf-8')
+            except:
+                continue
+            print(s)
+            if("Ready" in s) :
+                return
         exit()
     
     def show_state(self) :
@@ -63,7 +77,7 @@ class Arduino_Controler:
         print(msg)
 
     def LED_switch(self,pin,light) :
-        lm = "" if(light == 1) else "#"
+        lm = "" if(light == 1) else "~"
         self.message += lm + str(pin) + "," + lm
 
     def LED_swap(self,epin) :
@@ -71,8 +85,16 @@ class Arduino_Controler:
         self,LEDdatabase[pin] = 0 if(self,LEDdatabase[pin] == 1) else 1
    
 
-x = Arduino_Controler("COM3")
+x = Arduino_Controler("COM7")
 
 
+for i in range(24) :
+    x.LED_switch(i,1)
+    time.sleep(1)
+
+
+for i in range(24) :
+    x.LED_switch(i,0)
+    time.sleep(1)
 
 
