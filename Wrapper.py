5573825bf7a7f5894c6978be305b147eb60dc9b7
メジロ <meziro0415.dai@gmail.com>
2019-10-09 12:20:41 +0900

    test

--- Wrapper.py
+++ Wrapper.py
@@ -49,7 +49,7 @@ class Arduino_Controler:
 
             if result:
                 for x in result:
-                    self.switch[int(x[0])] = int(x[1])
+                    self.switch[int(x[0])] = 0 if (x[1] == '1') else 1
             
             #Send data
             if(self.message == "") :
--- make_notes.py
+++ make_notes.py
@@ -7,6 +7,7 @@ import threading
 s = w.Arduino_Controler("COM4")
 
 
+Flag = True
 
 def play_music():
     music_filename = "hyperspeed.mp3"
@@ -14,13 +15,32 @@ def play_music():
     pygame.mixer.music.load(music_filename)
     mp3_length = mp3(music_filename).info.length
     pygame.mixer.music.play(1)
-    time.sleep(mp3_length + 0.25)
+    time.sleep(10)
     pygame.mixer.music.stop()
+    global Flag
+    Flag = False
 
 thread = threading.Thread(target = play_music)
 thread.deamon = True
 thread.start()
 
+print("test")
 
-:q
+notes = []
+while Flag:
+    notes.append(s.switch.copy())
+    time.sleep(0.1)
+    s.show_state()
 
+print("test2")
+
+def print_state(x) :
+    for i in range(4) :
+        print(str(i + 10) + " : " + str(x[i + 10]),end = " ")
+    print()
+
+for i in notes:
+    #print_state(i)
+    pass
+
+s.exit_code = False 
