953ede31f69805d3a3ea78d6e9bd8686669a635d
メジロ <meziro0415.dai@gmail.com>
2019-11-01 19:00:46 +0900

    譜面

---
+++ Make_notes/Make_notes/Make_notes/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Make_notes {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Button1_KeyUp);
+            this.button1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Button1_PreviewKeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Make_notes/Make_notes/Make_notes/Form1.cs
@@ -0,0 +1,197 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Make_notes {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            bitmap = new Bitmap(this.Width, this.Height);
+            g = Graphics.FromImage(bitmap);
+
+            timer.Interval = 200;
+            timer.Tick += TickEvent;
+        }
+
+        WaveOutEvent device;
+        AudioFileReader audio;
+        Timer timer = new Timer();
+        int[] KeyState = new int[4];
+        int[] KeyFlag = new int[4];
+        int[] Sum = new int[4];
+        List<int[]> Data = new List<int[]>();
+        List<int[]> Sums = new List<int[]>();
+        Bitmap bitmap;
+        Graphics g;
+        bool FinishFlag = false;
+
+        private void Button1_Click(object sender, EventArgs e) {
+            if (device == null) {
+                device = new WaveOutEvent();
+                device.PlaybackStopped += OnPlaybackStopped;
+            }
+
+            if (audio == null) {
+                audio = new AudioFileReader(@"C:\Users\meziro\Quad-Bullet\firefly.mp3");
+                device.Init(audio);
+            }
+
+            //Limit = (int)(length / timer.Interval * 1000);
+
+            device.Play();
+            timer.Enabled = true;
+            timer.Start();
+
+            KeyFlag = new int[4] { 0, 0, 0, 0 };
+        }
+
+
+        /* Sounds */
+
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args) {
+            device.Dispose();
+            device = null;
+            audio.Dispose();
+            audio = null;
+
+            FinishFlag = true;
+        }
+
+
+        private void TickEvent(object _,EventArgs e) {
+            Data.Add(KeyState);
+            for (int i = 0; i < 4; i++)
+                ;// Console.Write(KeyState[i] + " ");
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 4; i++)
+                Sum[i] += KeyState[i];
+
+            for (int i = 0; i < 4; i++)
+                Console.Write(Sum[i] + " ");
+
+
+            Sums.Add(Sum);
+
+            KeyState = new int[4] { 0,0,0,0};
+
+            if(FinishFlag) {
+                timer.Stop();
+
+                WriteJson();
+            }
+        }
+
+        private void WriteJson() {
+            String output = "[";
+            int l = Data.Count;
+            for (int i = 0; i < l; i++) {
+                output += "{" + '"' + i + '"' + " : " + $"[{Data[i][0]},{Data[i][1]},{Data[i][2]},{Data[i][3]}]" + '}';
+                if (i != l - 1) output += ",";
+            }
+            output += "]";
+
+            Console.WriteLine(output);
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e) {
+            if (new List<Keys> { Keys.Return, Keys.Space }.Contains(e.KeyCode)) return;
+
+            switch(e.KeyCode) {
+                case Keys.A:
+                    if (KeyFlag[0] == 0) {
+                        KeyState[0] = 1;
+                        KeyFlag[0] = 1;
+                    }
+                    break;
+                case Keys.S:
+                    if (KeyFlag[1] == 0) {
+                        KeyState[1] = 1;
+                        KeyFlag[1] = 1;
+                    }
+                    break;
+                case Keys.D:
+                    if (KeyFlag[2] == 0) {
+                        KeyState[2] = 1;
+                        KeyFlag[2] = 1;
+                    }
+                    break;
+                case Keys.F:
+                    if (KeyFlag[3] == 0) {
+                        KeyState[3] = 1;
+                        KeyFlag[3] = 1;
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e) {
+            if (new List<Keys> { Keys.Return, Keys.Space }.Contains(e.KeyCode)) return;
+
+            switch (e.KeyCode) {
+                case Keys.A:
+                    if (KeyState[0] == 1)
+                        KeyState[0] = 0;
+                    else if (Sum[0] == 1) {
+                        KeyState[0] = -1;
+                        KeyFlag[0] = 0;
+                    }
+                    break;
+                case Keys.S:
+                    if (KeyState[1] == 1)
+                        KeyState[1] = 0;
+                    else if (Sum[1] == 1) {
+                        KeyState[1] = -1;
+                        KeyFlag[1] = 0;
+                    }
+                    break;
+                case Keys.D:
+                    if (KeyState[2] == 1)
+                        KeyState[2] = 0;
+                    else if (Sum[2] == 1) {
+                        KeyState[2] = -1;
+                        KeyFlag[2] = 0;
+                    }
+                    break;
+                case Keys.F:
+                    if (KeyState[3] == 1)
+                        KeyState[3] = 0;
+                    else if (Sum[3] == 1) {
+                        KeyState[3] = -1;
+                        KeyFlag[3] = 0;
+                    }
+                    break;
+            }
+        }
+
+
+        private void Button1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e) {
+            this.Form1_PreviewKeyDown(sender, e);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e) {
+            g.Clear(this.BackColor);
+
+            for(int i = 0;i < 4;i++) {
+                if(Sum[i] == 1) {
+                    g.FillRectangle(Brushes.Black,i * 50 + 50, 50, 50, 50);
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Button1_KeyUp(object sender, KeyEventArgs e) {
+            this.Form1_KeyUp(sender, e);
+        }
+    }
+
+    class Note {
+        int[] Element = { 0 };
+    }
+}
